85e240abba4e11b84206b868ed45c0593651e57b
Jake Nabasny <jake@nabasny.com>
2024-12-25 16:33:42 -0600

    hardcode php values

--- install.sh
+++
@@ -1,9 +0,0 @@
-kubectl create ns authentik
-helm repo add mariadb-operator https://helm.mariadb.com/mariadb-operator
-helm repo add authentik https://charts.goauthentik.io
-helm repo update
-helm install mariadb-operator-crds mariadb-operator/mariadb-operator-crds
-helm install mariadb-operator mariadb-operator/mariadb-operator
-helm install openldap helm-openldap/openldap-stack-ha --values openldap/values.yaml
-helm install redis oci://registry-1.docker.io/bitnamicharts/redis-cluster --set "metrics.enabled=true,cluster.nodes=3,cluster.replicas=0"
-helm install -n authentik authentik authentik/authentik -f authentik/values.yaml
